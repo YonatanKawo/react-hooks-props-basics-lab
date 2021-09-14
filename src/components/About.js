db54768908cd3035d2f1a336de6da72285a7fa05
Yonatan Kawo <yonas478@gmail.com>
2021-09-13 19:49:10 -0500

    commit tree 87135c7336abb38b6221a19827e283bbebeb930f

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const isbio = props.bio;
   return (
     <div id="about">
+
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <b>{isbio ? <p>{isbio}</p> : null} </b>
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
+      
+
     </div>
   );
 }
 
+
 export default About;
+
